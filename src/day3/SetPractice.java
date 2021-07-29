65c770cd1ac054318fb0949fffc35f82ff1a0d5a
mlc217 <mlc217@yahoo.com>
2021-07-29 15:25:48 -0400

    started set

--- src/day3/SetPractice.java
+++ src/day3/SetPractice.java
@@ -1,22 +1,7 @@
 package day3;
 
 public class SetPractice {
-    public static void main(String[] args) {
-
-        //Set is a collection type that only store unique item
-        // if you add duplicates
-        // its  a collection of unique elements
-        //set is done
-
-
-
-
-
-
-
-    }
-
-
+    //started set practice
 
 
 
