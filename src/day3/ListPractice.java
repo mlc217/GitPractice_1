fecf6ecdae4e5388a2bacf197ddc359cdcc60372
mlc217 <mlc217@yahoo.com>
2021-07-29 14:35:08 -0400

    started new class

---
+++ src/day3/ListPractice.java
@@ -0,0 +1,14 @@
+package day3;
+
+import java.util.ArrayList;
+
+public class ListPractice {
+
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+
+
+    }
+}
